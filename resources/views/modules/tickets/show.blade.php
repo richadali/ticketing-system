0fc5ba71a9b40b4281a1c60bb1880a3a9b889418
richadali <richadyaminali@gmail.com>
2025-08-14 03:45:06 +0530

    fix: Allow HTML rendering in ticket description

--- resources/views/modules/tickets/show.blade.php
+++ resources/views/modules/tickets/show.blade.php
@@ -177,7 +177,7 @@
                                 <strong>Description</strong>
                             </div>
                             <div class="card-body">
-                                <p>{{ $ticket->description }}</p>
+                                <p>{!! $ticket->description !!}</p>
                             </div>
                         </div>
                     </div>
